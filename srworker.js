1e11d704d2857ae660219626c833515fa464d577
Muhsinetr <72331917+Muhsinetr@users.noreply.github.com>
2024-11-15 21:25:30 +0530

    Update srworker.js

--- srworker.js
+++ srworker.js
@@ -1,7 +1,7 @@
 self.addEventListener("install",e=>{
     e.waitUntil(
         caches.open("static").then(cache=>{
-            return cache.addAll(["./","style.css","script.js","down-button.png","up-button.png","iqbalLog515.png", "iqbalLog 160.png"]);
+            return cache.addAll(["./","style.css","down-button.png","up-button.png","iqbalLog515.png", "iqbalLog 160.png"]);
         })
     )
 })
